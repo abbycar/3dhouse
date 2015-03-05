5a6b96647c90776278943f512bf838ebe389b34a
Abigail Carey <abigailcarey123@gmail.com>
2015-03-05 04:20:35 -0800

    Updated top down and fixed merge issue
    updated topdown selector so that it will go back to the exact same view
    no matter how much you move the camera around.
    
    Top down is now looking cute
    
    Temporarily added in axis for spatial reasoning purposes

--- house.js
+++ house.js
@@ -13,23 +13,31 @@ function init()
 	var div = document.getElementById("container");
 
 	renderer = new THREE.WebGLRenderer({ antialias:true });
-	renderer.setSize(window.innerWidth,window.innerHeight);
+	renderer.setSize(width,height);
 
 	div.appendChild(renderer.domElement);
 	scene = new THREE.Scene();
 
-	camera.position.y = 100;
-	camera.position.x = 0;
-	camera.position.z = 200;
-
+	// add camera to the scene
+	camera = new THREE.PerspectiveCamera(45,width/height,0.1,1000);
+	camera.position.y = 240;
+	camera.position.x = 20;
+	camera.position.z = -20;
+	scene.add(camera);
+	var axes = new THREE.AxisHelper(100);
+	scene.add( axes );
+	// add directional light
 	/*var directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
 
     var ambientLight = new THREE.AmbientLight(0Xffffff);
     ambientLight.position.set(0,1000,0);
     scene.add(ambientLight);*/
 
+    // add a light above the scene to light up the whole scene
 	/*var hLight = new THREE.HemisphereLight(0x666666,0x555555,1.0);
-
+	scene.add(hLight);
 
 	scene.add( new THREE.AmbientLight( 0x505050 ) );
 
@@ -64,9 +72,7 @@ function init()
     // loader to load in house model
     var loader = new THREE.OBJMTLLoader();
 	loader.load('model/house.obj','model/house.mtl',function(object){
-		object.position.x = -30;
-		object.position.y = 0;
-		object.position.z = 0;
+	    object.position.x = -10;
 		object.traverse(function(node){
 				if (node.material){
 					node.material.side = THREE.DoubleSide;					}
@@ -82,38 +88,22 @@ function init()
 //	ground = new THREE.Mesh(new THREE.PlaneGeoemtry(500,500,1),groundMaterial);
 //	scene.add(ground);
 	
-		// add directional light
-	var directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
-
-    // add a light above the scene to light up the whole scene
-	var hLight = new THREE.HemisphereLight(0x999999,0x555555,0.8);
-	scene.add(hLight);
-	// add top-down camera to the scene
-	camera = new THREE.PerspectiveCamera(40,width/height,1,1000);
-	scene.add(camera);
-	camera.position.y = 100;
-	camera.position.x = 0;
-	camera.position.z = 0;
-
-	
     // controls to rotate around the object right now
-	controls = new THREE.OrbitControls( camera );
-	controls.addEventListener('change',updateControls);
+	controls = new THREE.OrbitControls(camera);
+	//controls.addEventListener('change',updateControls);
 
 	// add window resize controller
 	window.addEventListener( 'resize', onWindowResize, false );
 	
-	
-	// Hold default values for the GUI
+		// Hold default values for the GUI
 	var guiConfigData = function() {
 		this.showControls = function() {
 		};
 		this.intensity = 1; // light intensity
 		this.cameraView = "Top-down"; // defaults to "top-down" view
 	};
-  
+	
+
 	// Create the GUI frame
 	var  guiConfig = new guiConfigData(  );
 	var gui = new dat.GUI( );
@@ -121,33 +111,33 @@ function init()
 	// Drop down menu to pick the view of the camera
 	gui.add(guiConfig, 'cameraView', [ 'Top-down', 'First-person' ] ).name("Camere View").onChange( function() {
 		if( guiConfig.cameraView == 'Top-down') { // camera angle for top-down view
-			controls = new THREE.OrbitControls( camera );
-			camera.position.y = 100;
-			camera.position.x = 0;
-			camera.position.z = 0;
+			controls.reset();
+			controls.enabled = true;
+			camera.position.y = 240;
+			camera.position.x = 20;
+			camera.position.z = -20;
 		} else if ( guiConfig.cameraView == 'First-person') { // camera position for first person point of view
-			//controls = new THREE.PointerLockControls(camera);
-			camera.position.y = 50;
-			camera.position.x = 0;
-			camera.position.z = 0;
-			camera.lookAt(-30,20,10);
+			//var controls = new THREE.PointerLockControls(camera);
+			controls.enabled = false;
+			camera.position.y = 40;
+			camera.position.x = 20;
+			camera.position.z = -20;
+
 		}
 	});	
 	
 	// Slide bar used for changing light intensity - in lighting folder
-	gui.add(directionalLight, 'intensity', 0, 1);
+	var intens = gui.add( guiConfig, 'intensity' ).min(0).max(1).step(.1).listen();
+	
 	// Show the control instructions in a pop up if clicked
 	gui.add( guiConfig, 'showControls').name("Show Controls").onChange( function() {
 		alert("Left Click + Move mouse = Rotate" + "\n" + "Right Click + Move mouse = Pan" + 
 		"\n" + "Scroll up = Zoom in" + "\n" + "Scroll down = Zoom out");
-	} ); 	
-		// axes
-	var axes = new THREE.AxisHelper(100);
-	scene.add( axes );
-}
-
-
+	} );
 	
+
+}
+		
 function updateControls(){
 	controls.update();
 }
@@ -156,7 +146,6 @@ function animate()
 {
 	requestAnimationFrame(animate);
 	renderer.render(scene,camera);
-	updateControls();
 }		
 
 function onWindowResize( e ) {
@@ -166,12 +155,6 @@ function onWindowResize( e ) {
 	camera.aspect = containerWidth / containerHeight;
 	camera.updateProjectionMatrix();
 }
-		
-		
-
-
-		
-		
 		
 		
 			
