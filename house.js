f16ba695ca6ca0d745948a64730f505f261be369
Abigail Carey <abigailcarey123@gmail.com>
2015-03-06 18:05:03 -0800

    Mouse hover can now change floor colors
    Temporarily set to make the hovered floor yellow. Still trying to work
    out getting text on the screen.
    
    Updated to latest version of Three.js

--- house.js
+++ house.js
@@ -8,26 +8,27 @@
 
 
 var RENDER_WIDTH = window.innerWidth, RENDER_HEIGHT = window.innerHeight;
-var controls,renderer,scene,camera;
+var controls,renderer,scene,camera, container;
 var ground,houseContainer;
 var groundMaterial;
-var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
+var mouse = { x: 0, y: 0 }, INTERSECTED;
 
 var clock = new THREE.Clock();
+var canvas1, context1, texture1;
 init();
 animate();
 
 
 function init()
 {
-	var div = document.getElementById("container");
+	container = document.getElementById("container");
 
 	renderer = new THREE.WebGLRenderer({ antialias:true });
 	renderer.setSize(RENDER_WIDTH,RENDER_HEIGHT);
 	renderer.domElement.style.backgroundColor = '#000000';
 	document.body.appendChild( renderer.domElement );
 
-	div.appendChild(renderer.domElement);
+	container.appendChild(renderer.domElement);
 	scene = new THREE.Scene();
 	// add camera to the scene
 	
@@ -218,6 +219,31 @@ function init()
 	
 	
 	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////       GUI             ///////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////
@@ -282,7 +308,52 @@ function init()
 		);
 	} );
 	
+	
+	// when the mouse moves, call the given function
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	
 
+		/////// draw text on canvas /////////
+
+	// create a canvas element
+	canvas1 = document.createElement('canvas');
+	context1 = canvas1.getContext('2d');
+	context1.font = "Bold 20px Arial";
+	context1.fillStyle = "rgba(0,0,0,0.95)";
+    context1.fillText('Hello, world!', 0, 20);
+    
+	// canvas contents will be used for a texture
+	texture1 = new THREE.Texture(canvas1) 
+	texture1.needsUpdate = true;
+	
+	
+	var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true} );
+	sprite1 = new THREE.Sprite( spriteMaterial );
+	sprite1.scale.set(200,100,1.0);
+	sprite1.position.set( 50, 50, 0 );
+	scene.add( sprite1 );	
+	
+
+	
+	
+	
+	
+	
+	
+}
+
+	function onDocumentMouseMove( event ) 
+{
+	// the following line would stop any other event handler from firing
+	// (such as the mouse's TrackballControls)
+	//event.preventDefault();
+
+	// update sprite position
+	sprite1.position.set( event.clientX, event.clientY - 20, 0 );
+	
+	// update the mouse variable
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
 
@@ -294,6 +365,67 @@ function animate()
 }		
 
 function render() {
+		// create a Ray with origin at the mouse position
+	//   and direction into the scene (camera direction)
+	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+	vector.unproject(camera );
+	var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+	// create an array containing all objects in the scene with which the ray intersects
+	var intersects = ray.intersectObjects( scene.children );
+
+	// INTERSECTED = the object in the scene currently closest to the camera 
+	//		and intersected by the Ray projected from the mouse position 	
+	
+	// if there is one (or more) intersections
+	if ( intersects.length > 0 )
+	{
+		// if the closest object intersected is not the currently stored intersection object
+		if ( intersects[ 0 ].object != INTERSECTED ) 
+		{
+		    // restore previous intersection object (if it exists) to its original color
+			if ( INTERSECTED ) 
+				INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+			// store reference to closest object as current intersection object
+			INTERSECTED = intersects[ 0 ].object;
+			// store color of closest object (for later restoration)
+			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+			// set a new color for closest object
+			INTERSECTED.material.color.setHex( 0xffff00 );
+			
+			// update text, if it has a "name" field.
+			if ( intersects[ 0 ].object.name )
+			{
+			    context1.clearRect(0,0,640,480);
+				var message = intersects[ 0 ].object.name;
+				var metrics = context1.measureText(message);
+				var width = metrics.width;
+				context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
+				context1.fillRect( 0,0, width+8,20+8);
+				context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+				context1.fillRect( 2,2, width+4,20+4 );
+				context1.fillStyle = "rgba(0,0,0,1)"; // text color
+				context1.fillText( message, 4,20 );
+				texture1.needsUpdate = true;
+			}
+			else
+			{
+				context1.clearRect(0,0,300,300);
+				texture1.needsUpdate = true;
+			}
+		}
+	} 
+	else // there are no intersections
+	{
+		// restore previous intersection object (if it exists) to its original color
+		if ( INTERSECTED ) 
+			INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+		// remove previous intersection object reference
+		//     by setting current intersection object to "nothing"
+		INTERSECTED = null;
+		context1.clearRect(0,0,300,300);
+		texture1.needsUpdate = true;
+	}
 	controls.update( clock.getDelta() );
 	renderer.render( scene, camera );
 }
